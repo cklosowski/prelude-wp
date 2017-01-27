04724896a279a5423229923724910da1737800c0
Alexander Persky <alexander@factor1studios.com>
2017-01-27 12:30:23 -0700

    comments_popup always outputs to the screen, so printf/sprintf wont work here

--- parts/meta.php
+++ parts/meta.php
@@ -6,9 +6,12 @@
 
 <div class="meta">
   <span><?php the_time( 'F jS, Y' ); ?></span>
+
   <?php
   if ( comments_open() ) :
-    echo sprintf('| <span>%s<span>', comments_popup_link( 'No Comments Yet', '1 Comment', '', 'comments-link', 'No Comments Allowed' ));
+    echo '| <span>';
+    comments_popup_link( 'No Comments Yet', '1 Comment', '', 'comments-link', 'No Comments Allowed' );
+    echo '</span>';
   endif;
   ?>
 </div>
