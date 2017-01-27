b050bf772ebc329010a72d2198aca02c8e67795e
Alexander Persky <alexander@factor1studios.com>
2017-01-27 12:43:28 -0700

    JS formatting and use shorthand jQuery ready call

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,12 @@
-jQuery( document ).ready(function( $ ) {
-  // Inside of this function, $() will work as an alias for jQuery()
-  // and other libraries also using $ will not be accessible under this shortcut
-  // https://codex.wordpress.org/Function_Reference/wp_enqueue_script#jQuery_noConflict_Wrappers
+jQuery(function($) {
+  'use strict';
+
+  var isTouchDevice;
 
   // Touch Device Detection
-	var isTouchDevice = 'ontouchstart' in document.documentElement;
-	if( isTouchDevice ) {
-		$('body').removeClass('no-touch');
-	}
-  
+  isTouchDevice = 'ontouchstart' in document.documentElement;
+
+  if (isTouchDevice) {
+    $('body').removeClass('no-touch');
+  }
 });
