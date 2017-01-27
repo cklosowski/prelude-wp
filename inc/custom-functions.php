8a601751666d5dc9a22965a9b1f4c6bf90ac594a
Alexander Persky <alexander@factor1studios.com>
2017-01-27 15:26:26 -0700

    Update custom functions

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -3,10 +3,22 @@
  * Custom Functions
  */
 
+/**
+ * Debug some piece of code.
+ * Remove the die; if you want the rest of the code on the page to execute
+ *
+ * @param $code The code that you want to check.
+ */
+function debug( $code ) {
+  printf( '<pre>%s</pre>', print_r( $code, true ) );
+  die;
+}
+
 // Get featured image as URL
 function featuredURL( $size = 'full' ) {
   $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size );
   $url   = $thumb['0'];
+
   echo $url;
 }
 
@@ -14,6 +26,7 @@ function featuredURL( $size = 'full' ) {
 function featuredBG( $size = 'full', $pos_x = 'center', $pos_y = 'center', $repeat = 'no-repeat' ) {
   $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size );
   $url   = $thumb['0'];
+
   echo 'style="background: url(' . $url . ')' . $pos_x . ' ' . $pos_y . ' ' . $repeat . '"';
 }
 
